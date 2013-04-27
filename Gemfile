d0bcdc586e0c0498077b31b3307c2798f9980753
Andrey 'lolmaus' Mikhaylov <lolmaus@gmail.com>
2013-04-27 20:33:08 +0400

    SASS compilation test

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
-source :rubygems
+source 'https://rubygems.org'
+
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'haml'
+gem 'sass'
--- test.rb
+++ test.rb
@@ -1,5 +1,7 @@
 require 'bundler/setup'
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'sass'
 require 'haml'
 
 get '/' do
@@ -8,6 +10,16 @@ end
 
 post '/result' do
   @html = params[:html]
-  @css =  params[:css]
+  #@css =  params[:css]
+
+
+  begin
+    @css = sass(params[:css].chomp, {:style => :nested, :quiet => true})
+
+  rescue Sass::SyntaxError => e
+    status 200
+    e.to_s
+  end
+
   haml :result, layout: false
 end
