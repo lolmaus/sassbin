7a7ab12d28bf84963d6c95f1385b509c8bc64c44
Andrey 'lomaus' Mikhaylov <lolmaus@gmail.com>
2013-05-29 17:00:27 +0400

    2-space indentation for Ace editors

--- app/js/app/editors/loadEditors.js
+++ app/js/app/editors/loadEditors.js
@@ -13,15 +13,20 @@ function loadEditors() {
     editorHtml.getSession().setMode("ace/mode/html");
     editorHtml.getSession().setValue($('#editor-html-stunt-double').text());
     editorHtml.getSession().setUseWrapMode(true);
+    editorHtml.getSession().setTabSize(2);
+    editorHtml.getSession().setUseSoftTabs(true);
 
     editorSass = ace.edit("editor-sass");
     editorSass.setTheme("ace/theme/github");
     editorSass.getSession().setMode("ace/mode/sass");
-    editorSass.getSession().setUseWrapMode(true);
+    editorSass.getSession().setTabSize(2);
+    editorSass.getSession().setUseSoftTabs(true);
 
     editorCss = ace.edit("editor-css");
     editorCss.setTheme("ace/theme/github");
     editorCss.getSession().setMode("ace/mode/css");
     editorCss.getSession().setUseWrapMode(true);
+    editorCss.getSession().setTabSize(2);
+    editorCss.getSession().setUseSoftTabs(true);
     editorCss.setReadOnly(true);
 }
